8cfae8d8e5777c5e1d1539999f31a205f58bf3f1
Nidhish Bisht <66567464+NIDHISHBISHT@users.noreply.github.com>
2022-07-13 13:31:08 +0530

    added

---
+++ StudentInformationSystem/src/model/Account.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Account {
+int AccountNo;
+
+public int getAccountNo() {
+	return AccountNo;
+}
+
+public void setAccountNo(int accountNo) {
+	AccountNo = accountNo;
+}
+
+}
--- StudentInformationSystem/src/model/Customer.java
+++ StudentInformationSystem/src/model/Customer.java
@@ -1,5 +1,14 @@
 package model;
 
 public class Customer {
+int customerAge;
+
+public int getCustomerAge() {
+	return customerAge;
+}
+
+public void setCustomerAge(int customerAge) {
+	this.customerAge = customerAge;
+}
 
 }
