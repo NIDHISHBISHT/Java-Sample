6f76bf338ba15aed512594b407cd68ce97242f6c
Nidhish Bisht <66567464+NIDHISHBISHT@users.noreply.github.com>
2022-07-13 13:41:08 +0530

    edited

--- StudentInformationSystem/src/model/Account.java
+++ StudentInformationSystem/src/model/Account.java
@@ -2,6 +2,15 @@ package model;
 
 public class Account {
 int AccountNo;
+int ifscCode;
+
+public int getIfscCode() {
+	return ifscCode;
+}
+
+public void setIfscCode(int ifscCode) {
+	this.ifscCode = ifscCode;
+}
 
 public int getAccountNo() {
 	return AccountNo;
