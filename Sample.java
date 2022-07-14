a1be24b288ad337f15783ba29ea34b9044a5ba6c
Nidhish Bisht <66567464+NIDHISHBISHT@users.noreply.github.com>
2022-07-14 16:35:36 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,4 +1,9 @@
-class Sample{
-public static void main(String[] args){
-System.out.println("Nidhish Bisht");}
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+
 }
